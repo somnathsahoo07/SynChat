42e65ca9facd82344dc961b42b957bfcfa1e6df7
somanthsahoo07 <somsahoo0703@gmail.com>
2025-07-07 12:59:46 +0530

    Increase request body size limits for JSON and URL-encoded data

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({limit:"20mb"}));
+app.use(express.urlencoded({extended:true, limit:"20mb"}))
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
